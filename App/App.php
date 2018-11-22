02fe72c872a04c64f4624daebcd823ecc49f87b4
Thiagopg7 <44246761+Thiagopg7@users.noreply.github.com>
2018-11-22 16:24:43 -0200

    Adicinado mais rotas e redirecionado botão painel

--- App/App.php
+++ App/App.php
@@ -159,7 +159,6 @@ class App
             "relatorio"=>"relatorios",
             "controleusuarios"=>"usuario/cadastro",
             "config"=>"configuracoes"
-            
         ];
 
             
--- App/Views/layouts/aside.php
+++ App/Views/layouts/aside.php
@@ -1,13 +1,10 @@
 <section class="columns is-fullheight">
   
   <aside class="column is-2 aside menu is-fullheight">
-    
-    
     <ul class="menu-list ">
         <p class="menu-label"><label data-tag="administracao"></p>
         <li class="menu-item"><a href="http://localhost/projeto-final/"><span class="icon is-small"><i class="fas fa-tachometer-alt"></i></span><span data-tag="painel"></span></a></li>
         <li class="menu-item"><a <?php if($viewVar['nameController'] == "ApropriacaoController") { ?> class="is-active" <?php } ?> href="http://<?php echo APP_HOST; ?>/apropriacao/servicoBusca"><i class="fas fa-briefcase"></i> <span data-tag="servico"></span></a></li>
-
             <li><a><span class="icon is-small"><i class="fas fa-edit"></i></span> <span data-tag="cadastros"></span></a>
               <ul>
                   <!-- <li class="menu-item"><a>Equipamentos</a></li> -->
